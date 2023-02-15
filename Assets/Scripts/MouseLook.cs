3c06e7803904751c8ac6d1603478ef1be5387699
FrancisBDJ <francisbartholomewdaniele@paucasesnovescifp.cat>
2023-02-15 11:47:25 +0100

    MouseLook y PlayerMovement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private Vector3 _angles = Vector3.zero;
+    private readonly float _maxAngles = 90.0f;
+
+    [SerializeField]private Transform _playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        float rotateHorizontal = Input.GetAxis("Mouse X");
+        float rotateVertical = Input.GetAxis("Mouse Y");
+                
+        _angles.y += rotateHorizontal ;
+         
+                            
+        _angles.x -= rotateVertical ;
+        _angles.x = Math.Clamp(_angles.x, -_maxAngles, _maxAngles);
+                            
+        _playerTransform.transform.rotation = Quaternion.Euler(_angles);            
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private CharacterController controller;
+    private Vector3 playerVelocity;
+    
+    // GroundCheck
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f; //Umbral de distància enterra
+    public LayerMask ground;
+    private float playerSpeed = 12.0f;
+    private float jumpHeight = 2.0f;
+    public float gravity = -9.81f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance,ground);
+        Debug.Log(isGrounded);
+        
+        
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move* playerSpeed * Time.deltaTime);
+
+        
+        
+
+        playerVelocity.y += gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        }
+    }
+}

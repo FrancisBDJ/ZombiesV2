8d56e4820ab0d9d2a10e03c530c68c345fa12958
FrancisBDJ <francisbartholomewdaniele@paucasesnovescifp.cat>
2023-04-20 13:47:19 +0200

    Corregidos bugs Sonido y de compilacion de Build

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioListener.volume = 1;
         isPaused = false;
         isDead = false;
         pausePanel.SetActive(false);
